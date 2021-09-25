389f472b27f4d26b6d524e15287b69b0a3b1d4aa
Jason Sturges <jason@jasonsturges.com>
2021-09-25 16:25:01 -0500

    Footer component

---
+++ src/components/Footer.svelte
@@ -0,0 +1,17 @@
+<script>
+  import Divider from './Divider.svelte'
+  import Social from './Social.svelte'
+</script>
+
+<style>
+  footer {
+    text-align: center;
+    margin-bottom: 96px;
+  }
+</style>
+
+<footer>
+  <Divider width={280} height={120} fill="#efefef" />
+
+  <Social />
+</footer>
