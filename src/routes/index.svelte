44a56c29e392a78c488498451b316829181b5de0
Jason Sturges <jason@jasonsturges.com>
2021-09-25 16:26:30 -0500

    Index meta

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,63 +1,67 @@
+<svelte:head>
+  <title>Jason Sturges</title>
+  <meta name="description"
+        content="Jason Sturges, avant-garde experimental artist.  Creative professional leveraging technology for immersive experiences - technologist; photographer; musician; explorer; human consciousness" />
+  <meta name="keyboards" content="artist, technologist, musician, photographer, jason, sturges" />
+</svelte:head>
+
 <script>
-	import AvantGarde from '../components/AvantGarde.svelte';
-	import Avatar from '../components/Avatar.svelte';
+  import AvantGarde from '../components/AvantGarde.svelte';
+  import Avatar from '../components/Avatar.svelte';
 </script>
 
 <style>
-	.cover {
-		padding: 20px;
-		width: 310px;
-		margin: 20px auto;
-		text-align: center;
-	}
+  .cover {
+    padding: 20px;
+    width: 310px;
+    margin: 20px auto;
+    text-align: center;
+  }
 
-	@media only screen and (min-width: 610px) {
-		.cover {
-			width: 610px;
-		}
-	}
+  @media only screen and (min-width: 610px) {
+    .cover {
+      width: 610px;
+    }
+  }
 
-	.cover .author {
-		margin: 0;
-		font-family: playfair-display, Arial, serif;
-		font-size: 2em;
-		letter-spacing: 0.65em;
-		text-transform: uppercase;
-		font-weight: 100;
-		line-height: 1em;
-		margin-bottom: 1.6em;
-	}
+  .cover .author {
+    margin: 0;
+    font-family: playfair-display, Arial, serif;
+    font-size: 2em;
+    letter-spacing: 0.65em;
+    text-transform: uppercase;
+    font-weight: 100;
+    line-height: 1em;
+    margin-bottom: 1.6em;
+  }
 
-	.cover .author:first-letter {
-		font-size: 2.2em;
-		display: inline-block;
-	}
+  .cover .author:first-letter {
+    font-size: 2.2em;
+    display: inline-block;
+  }
 
-	.cover .tagline {
-		font-family: cardo, serif;
-		font-style: normal;
-		font-weight: 400;
-		font-size: 1.2em;
-		letter-spacing: 0.2em;
-		margin: 1em 0;
-		text-transform: capitalize;
-	}
+  .cover .tagline {
+    font-family: cardo, serif;
+    font-style: normal;
+    font-weight: 400;
+    font-size: 1.2em;
+    letter-spacing: 0.2em;
+    margin: 1em 0;
+    text-transform: capitalize;
+  }
 
-	.cover .statement {
-		margin: 0 64px;
-	}
+  .cover .statement {
+    margin: 0 64px;
+  }
 </style>
 
-<svelte:head>
-	<title>Jason Sturges</title>
-</svelte:head>
-
 <section class="cover">
-	<p class="author">Jason Sturges</p>
-	<AvantGarde />
-	<p class="tagline">Experimental artist</p>
+  <p class="author">Jason Sturges</p>
+  <AvantGarde />
+  <p class="tagline">Experimental artist</p>
 
-	<Avatar />
+  <Avatar />
 
-	<p class="statement">Creative professional leveraging technology for immersive experiences &mdash; technologist; photographer; musician; explorer; human consciousness </p>
+  <p class="statement">Creative professional leveraging technology for immersive experiences &mdash; technologist;
+    photographer; musician; explorer; human consciousness</p>
 </section>
