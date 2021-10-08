6cb2555e96c1d8c9f72e5ef241a69db3337f1c1a
Jason Sturges <jason@jasonsturges.com>
2021-10-08 00:26:09 -0500

    Prototyping

---
+++ src/routes/work/photography/portrait.svelte
@@ -0,0 +1,3 @@
+<h1>Portrait Photography</h1>
+
+Featured in Gilded, Solstice, Stylecruze, Elegant, EVON, Horizont, Avessa, Boulevard, International Music magazines
--- src/routes/work/technology.svelte
+++ src/routes/work/technology.svelte
@@ -1,1 +1,5 @@
-technology
+
+<section class="heading center">
+  <h1>Technology</h1>
+</section>
+
