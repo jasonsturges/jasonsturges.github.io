2189e394cfcc0469b89ee5cd6359e5e0ed56be3a
Jason Sturges <jason@jasonsturges.com>
2021-10-08 00:24:56 -0500

    Navigation

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -50,9 +50,9 @@
 
 <nav>
   <ul>
-    <li><a aria-current="{segment === undefined ? 'page' : undefined}" href=".">home</a></li>
-    <li><a aria-current="{segment === 'work' ? 'page' : undefined}" href="work">work</a></li>
-    <li><a aria-current="{segment === 'about' ? 'page' : undefined}" href="about">about</a></li>
+    <li><a aria-current="{segment === undefined ? 'page' : undefined}" href=".">Home</a></li>
+    <li><a aria-current="{segment === 'work' ? 'page' : undefined}" href="work">Work</a></li>
+    <li><a aria-current="{segment === 'about' ? 'page' : undefined}" href="about">About</a></li>
 
     <!-- for the blog link, we're using rel=prefetch so that Sapper prefetches
          the blog data when we hover over the link or tap it on a touchscreen -->
