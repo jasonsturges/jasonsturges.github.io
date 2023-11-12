d59e73c9b366e93deb6137c50f80f939d08a011a
Jason Sturges <jason@jasonsturges.com>
2023-11-12 14:08:00 -0600

    Next config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  output: "export",
+  trailingSlash: true,
+  basePath: "",
+  images: {
+    unoptimized: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
