cdf4468fae263bbbbe2034737ead207c13abdb5b
Jason Sturges <jason@jasonsturges.com>
2023-11-13 00:22:01 -0600

    Window size hook

---
+++ src/hooks/useWindowSize.ts
@@ -0,0 +1,23 @@
+"use client";
+
+import { useState, useEffect } from "react";
+
+export const useWindowSize = () => {
+  const [width, setWidth] = useState(window.innerWidth);
+  const [height, setHeight] = useState(window.innerHeight);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWidth(window.innerWidth);
+      setHeight(window.innerHeight);
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return {
+    width: width,
+    height: height,
+  };
+};
