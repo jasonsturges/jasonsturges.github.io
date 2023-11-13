13fe26dd80321a75ae479dd5c0d2b2c278bdc06e
Jason Sturges <jason@jasonsturges.com>
2023-11-13 14:27:56 -0600

    Add tailwind typography

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import typography from "@tailwindcss/typography";
 
 export default {
   content: [
@@ -8,5 +9,5 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [typography],
 } satisfies Config;
