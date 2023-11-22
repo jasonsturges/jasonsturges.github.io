307d166b21671215508e38c1c08835753351bf62
Jason Sturges <jason@jasonsturges.com>
2023-11-22 00:51:27 -0600

    Astro site config

--- astro.config.ts
+++ astro.config.ts
@@ -6,6 +6,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
+  site: "https://jasonsturges.com",
   integrations: [mdx(), sitemap(), tailwind()],
 } satisfies AstroUserConfig);
