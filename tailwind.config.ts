02e3e0e82f32e3608e610a1c06dda4bf32a76b62
Jason Sturges <jason@jasonsturges.com>
2023-11-23 00:06:10 -0600

    Theme fonts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,14 @@
 import type { Config } from "tailwindcss";
+import theme from "tailwindcss/defaultTheme";
 import typography from "@tailwindcss/typography";
 
 export default {
   content: ["./src/**/*.{astro,md,mdx}"],
   darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: { sans: ["atkinson", ...theme.fontFamily.sans] },
+    },
   },
   plugins: [typography],
 } satisfies Config;
