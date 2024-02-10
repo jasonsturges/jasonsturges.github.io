5cdeb331524cd67415f16f8f89be7575de88f5cd
Jason Sturges <jason@jasonsturges.com>
2024-02-10 13:57:50 -0600

    Camera refactoring

--- src/features/roxik/camera.ts
+++ src/features/roxik/camera.ts
@@ -1,45 +1,52 @@
-import { IcosahedronGeometry, Mesh, MeshPhongMaterial, PerspectiveCamera, Vector3 } from "three";
+import { Clock, IcosahedronGeometry, Mesh, MeshPhongMaterial, PerspectiveCamera, Vector3 } from "three";
 import { models } from "./scene.ts";
 
+// Camera properties
+let targetModel: Mesh<IcosahedronGeometry, MeshPhongMaterial>;
+let speed = 0;
+let offset = 0;
+let distance = 6;
+let targetDistance = 0;
+let targetPosition = new Vector3(0, 0, 0);
+let targetSpeed = 0;
+let rotationAngle = 0;
+let rotationSpeed = 0.03;
+let timer = 0;
+const clock = new Clock();
+
+// Camera
 export const camera = new PerspectiveCamera(55, window.innerWidth / window.innerHeight, 0.001, 1000);
 camera.position.set(0, 0, 10);
-let targetModel: Mesh<IcosahedronGeometry, MeshPhongMaterial>;
-let frame = 1000;
-let sceneLimit = 90;
-let target = new Vector3(0, 0, 0);
-let cs = 0;
-let gy = 0;
-let l = 0;
-let bl = 6;
-let ts = 0;
-let r = 0;
-let rp = 0.03;
 
+// Change scene
+export const changeCamera = () => {
+  timer = clock.elapsedTime + Math.random() + 0.5;
+  targetModel = models[Math.floor(Math.random() * models.length)];
+  targetSpeed = 0;
+  speed = 0;
+  offset = Math.random() * 8 - 4;
+  rotationSpeed = Math.random() * 0.06 - 0.03;
+  distance = Math.random() * 4 + 7;
+};
+changeCamera();
+
+// Camera frame handler
 export const cameraFrameHandler = () => {
-  if (++frame > sceneLimit) {
-    frame = 0;
-    sceneLimit = Math.floor(Math.random() * 60 + 30);
-    targetModel = models[Math.floor(Math.random() * models.length)];
-    ts = 0;
-    cs = 0;
-    gy = Math.random() * 8 - 4;
-    rp = Math.random() * 0.06 - 0.03;
-    bl = Math.random() * 4 + 7;
-  }
+  const delta = clock.getDelta() * 35;
 
-  if (ts < 0.05) ts += 0.005;
-  if (cs < 0.5) cs += 0.005;
+  if (clock.elapsedTime > timer) changeCamera();
+  if (targetSpeed < 0.05) targetSpeed += 0.005 * delta;
+  if (speed < 0.5) speed += 0.005 * delta;
 
-  target.x += (targetModel.position.x - target.x) * ts;
-  target.y += (targetModel.position.y - target.y) * ts;
-  target.z += (targetModel.position.z - target.z) * ts;
+  rotationAngle += rotationSpeed * delta;
+  targetDistance += (distance - targetDistance) * 0.1 * delta;
 
-  camera.lookAt(target);
+  targetPosition.x += (targetModel.position.x - targetPosition.x) * targetSpeed * delta;
+  targetPosition.y += (targetModel.position.y - targetPosition.y) * targetSpeed * delta;
+  targetPosition.z += (targetModel.position.z - targetPosition.z) * targetSpeed * delta;
 
-  r += rp;
-  l += (bl - l) * 0.1;
-
-  camera.position.x += (Math.cos(r) * l + targetModel.position.x - camera.position.x) * cs;
-  camera.position.y += (targetModel.position.y + gy - camera.position.y) * cs;
-  camera.position.z += (Math.sin(r) * l + targetModel.position.z - camera.position.z) * cs;
+  camera.position.x += (Math.cos(rotationAngle) * targetDistance + targetModel.position.x - camera.position.x) * speed * delta;
+  camera.position.y += (targetModel.position.y + offset - camera.position.y) * speed * delta;
+  camera.position.z += (Math.sin(rotationAngle) * targetDistance + targetModel.position.z - camera.position.z) * speed * delta;
+  camera.lookAt(targetPosition);
 };
